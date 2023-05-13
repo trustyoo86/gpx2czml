c9a492ef8f80e2e99b708a758519ea34dea144e5
hanseungyoo <trustyoo86@gmail.com>
2023-05-14 01:17:04 +0900

    feat: Add typescript config, files

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  printWidth: 80,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
