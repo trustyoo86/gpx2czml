bf47e1407146ebe01920a93f3d210f26e8268511
HanseungYoo <trustyoo86@gmail.com>
2023-05-25 00:11:27 +0900

    feat: Add gpx module data

--- src/Gpx.ts
+++ src/Gpx.ts
@@ -1,16 +1,16 @@
+import ElementUtil from './ElementUtil';
+
 class Gpx {
   private hasDOMParser: boolean;
-  private isHttpRequest: boolean;
   constructor() {
     this.hasDOMParser = !!window.DOMParser;
-    this.isHttpRequest = !!window.XMLHttpRequest;
   }
 
   /**
    * Make window DOM parser
    * @returns window.DOMParser
    */
-  makeDOMParser(): typeof window.DOMParser {
+  makeDOMParser(): InstanceType<typeof window.DOMParser> {
     let tmp;
     if (this.hasDOMParser) {
       tmp = new window.DOMParser();
@@ -23,8 +23,37 @@ class Gpx {
    * Get xml http request
    * @returns window.XMLHttpRequest
    */
-  getHttpRequest() {
+  getHttpRequest(): InstanceType<typeof window.XMLHttpRequest> {
     return new window.XMLHttpRequest();
+  }
+
+  /**
+   * Parsing gpx element data
+   * @param data element string
+   * @returns any
+   */
+  parseGpx(data: string) {
+    try {
+      const element = this.getGpxEls(data);
+      /**
+       * @todo gpx elements return
+       */
+      console.log('element');
+    } catch (e) {
+      return e;
+    }
+  }
+
+  /**
+   * Get gpx elements
+   * @param data response gpx data string
+   * @returns Element
+   */
+  getGpxEls(data: string): Element {
+    const tmp = new window.DOMParser();
+    const xml = tmp.parseFromString(data, 'text/xml');
+
+    return xml.getElementsByTagName('gpx')[0];
   }
 }
 
