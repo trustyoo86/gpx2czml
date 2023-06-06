78ab8f0423e92b1176e07f13d29aa1520b068194
hanseungyoo <trustyoo86@gmail.com>
2023-06-06 18:39:56 +0900

    feat: add package refresh file

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-function test() {
+import { asyncFromApi } from './src/index';
 
+function main() {
+  asyncFromApi('./resources/584286793.gpx', (isError, data) => {
+    console.log('result', isError, data);
+  });
 }
 
-export default test;
+export default main();
